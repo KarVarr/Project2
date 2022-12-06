f45ede0863243d8af2c1e7c238c0d29cf450d17c
KarVarr <megatr9n@gmail.com>
2022-12-06 22:18:37 +0300

    added border for the buttons and a country names array

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -9,16 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var button1: UIButton!
-    
     @IBOutlet var button2: UIButton!
-    
     @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        countries += ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "Uk", "Us"]
+       
+        button1.layer.borderWidth = 3
+        button2.layer.borderWidth = 3
+        button3.layer.borderWidth = 3
+        button1.layer.borderColor = UIColor.blue.cgColor
+        
+        button2.layer.borderColor = UIColor.green.cgColor
+        button3.layer.borderColor = UIColor.red.cgColor
+        
+        
+        
+        askQuestion()
     }
-
+    func askQuestion()  {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 
 }
 
