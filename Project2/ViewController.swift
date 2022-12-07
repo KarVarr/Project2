d5699c8f2599f84413e19330be09c0c5c09658b2
KarVarr <megatr9n@gmail.com>
2022-12-07 16:51:30 +0300

    added shuffle array and random title from 0 to 2

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     
     override func viewDidLoad() {
@@ -21,12 +22,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         countries += ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "Uk", "Us"]
-       
-        button1.layer.borderWidth = 3
-        button2.layer.borderWidth = 3
-        button3.layer.borderWidth = 3
-        button1.layer.borderColor = UIColor.blue.cgColor
         
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.blue.cgColor
         button2.layer.borderColor = UIColor.green.cgColor
         button3.layer.borderColor = UIColor.red.cgColor
         
@@ -35,10 +35,14 @@ class ViewController: UIViewController {
         askQuestion()
     }
     func askQuestion()  {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
-
+    
 }
 
